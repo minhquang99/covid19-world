bbd4d11b705c1be5cbddfd2a17436657f9ce8369
ASUS <ASUS@LAPTOP-D8LBK4KJ>
2021-08-01 04:13:58 +0700

    show countries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import CountrySelector from "./components/CountrySelector";
+import HighLight from "./components/HighLight";
+import Chart from "./components/Chart";
+import { useEffect, useState } from "react";
+import { getCountries } from "./apis/api";
 
 
 function App() {
+
+  const [ countries, setCountries ] = useState([]);
+
+  useEffect(() => {
+    getCountries().then((res) => {
+      console.log(res); //axios là 1 promise => cần then() để lấy data trả về
+      setCountries(res.data);
+    })
+  }, []); //gọi API 1 lần duy nhất khi component được render lần đầu
+
   return (
-    <p>Covid App</p>
+    <div>
+      <p>Covid App</p>
+      <CountrySelector countries={countries}/>
+      <HighLight />
+      <Chart />
+    </div>
   );
 }
 
